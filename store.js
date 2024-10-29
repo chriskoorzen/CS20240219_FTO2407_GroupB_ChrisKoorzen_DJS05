aa5e97bce6b60d4dbaa59100508d633388a2071a
Chris Koorzen <fckoorzen@gmail.com>
2024-10-29 12:52:38 +0200

    Setup store skeleton

---
+++ store.js
@@ -0,0 +1,14 @@
+
+const createStore = (initial) => {
+
+    const states = [initial];
+    const subscribers = [];
+
+    const dispatch = (action) => {};
+
+    const getState = () => {};
+
+    const subscribe = (handler) => {};
+
+    return [dispatch, getState, subscribe];
+};
