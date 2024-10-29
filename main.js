8975868d47407ebd6abb26c021888957b73e7d3b
Chris Koorzen <fckoorzen@gmail.com>
2024-10-29 17:25:54 +0200

    Run project scenarios

--- main.js
+++ main.js
@@ -4,5 +4,29 @@ import {
     subtract,
     reset
 } from "./tally.js";
+import { sleep } from "./utility.js";
 
+// -- Init --
+const tallyStore = createStore(
+    { count: 0 }
+);
 
+// Scenario 1
+await sleep(1000);
+console.log("Scenario 1: ", tallyStore.getState());
+
+// Scenario 2
+await sleep(750);
+tallyStore.dispatch(add);
+tallyStore.dispatch(add);
+console.log("Scenario 2: ", tallyStore.getState());
+
+// Scenario 3
+await sleep(750);
+tallyStore.dispatch(subtract);
+console.log("Scenario 3: ", tallyStore.getState());
+
+// Scenario 4
+await sleep(750);
+tallyStore.dispatch(reset);
+console.log("Scenario 4: ", tallyStore.getState());
