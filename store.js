ffb8d610ff783f0eea6d4c0241772e3eb2bbb73c
Chris Koorzen <fckoorzen@gmail.com>
2024-11-04 12:16:14 +0200

    Implement methods to navigate through the history of the state store.

--- store.js
+++ store.js
@@ -1,32 +1,29 @@
 /**
  * @typedef {Object} GenericStore A generic store that keeps track of application state.
- * @prop {function(Dispatcher): [CurrentState, PreviousState]} dispatch Triggers the passed user-defined Dispatcher. Returns CurrentState and PreviousState.
+ * @prop {function(Dispatcher): [State, State]} dispatch Triggers the passed user-defined Dispatcher. Returns current State and previous State.
+ * @prop {function} rewind A method to rewind application state to an earlier version (if exists)
+ * @prop {function} forward A method to forward application state to a newer version (if exists)
  * @prop {function} getState Returns the current state of the application.
  * @prop {function} subscribe Registers a Subscriber. Returns the unsubscribe function bound to the Subscriber passed
  * @prop {function} unsubscribe Removes a Subscriber.
  */
 /**
- * @typedef {Object} CurrentState
- * A user-defined object that represents the current state of the application
- */
-
-/**
- * @typedef {Object} PreviousState
- * A user-defined object that represents the previous state of the application
+ * @typedef {Object} State
+ * A user-defined object that represents the state of the application
  */
 /**
  * A user-defined function that accepts the current state of the application, modifies it in some way (or not)
  * and returns the updated state of the application.
  * @callback Dispatcher
- * @param {CurrentState} currentState
- * @returns {CurrentState}
+ * @param {State} currentState 
+ * @returns {State} Newly updated State object
  */
 /**
  * A callback function that wants to stay updated on any changes in application state. On state change, the current and previous
  * (immutable) states of the application is passed to this callback.
  * @callback Subscriber
- * @param {CurrentState} currentState
- * @param {PreviousState} previousState
+ * @param {State} currentState
+ * @param {State} previousState
  */
 
 
@@ -39,46 +36,70 @@
 export const createStore = (initial) => {
 
     const states = [Object.freeze(initial)];
+    let history = 0;
     const subscribers = new Set();
 
-    
+
     /**
-     * Triggers the passed user-defined Dispatcher. Returns CurrentState and PreviousState.
+     * Triggers the passed user-defined Dispatcher. Returns current State and previous State to the dispatcher.
      *
      * @param {Dispatcher} action
-     * @returns {[CurrentState, PreviousState]}
+     * @returns {[State, State]}
      */
     const dispatch = (action) => {
         if(typeof action !== "function"){
             throw new Error("action is required to be a function");
         };
 
+        /**
+         * If the user navigated away from the most current state, and calls a dispatch,
+         * we must prune the state-branch at that position, and start a new history.
+         */
+        if (history !== (states.length-1)){
+            states.splice(history+1, (states.length-history));
+        };
+
         const prev = getState();
         const next = Object.freeze(action(prev));
 
-        subscribers.forEach(
-            (handler) => { 
-                try {                       // Subscriber failure shouldn't break state updates
-                    handler(next, prev); 
-                } catch (error){
-                    console.error("Subscriber handler function failed on dispatch.", error);
-                };
-            }
-        );
-        
-        states.push(next);
+        _trigger_subscribers(next, prev);
+
+        history = states.push(next) - 1;
 
         return [next, prev];
+    };
+
+
+    /** A method to rewind application state to an earlier version (if exists) */
+    const rewind = () => {
+        if (history === 0) return;
+
+        const prev = getState();
+        history -= 1;
+        const next = getState();
+
+        _trigger_subscribers(next, prev);
+    };
+
+    /** A method to forward application state to a newer version (if exists) */
+    const forward = () => {
+        if (history === (states.length-1)) return;
+
+        const prev = getState();
+        history += 1;
+        const next = getState();
+
+        _trigger_subscribers(next, prev);
     };
 
     
     /**
      * Returns the current state of the application.
      *
-     * @returns {CurrentState}
+     * @returns {State}
      */
     const getState = () => {
-        return states[states.length -1];
+        return states[history];
     };
 
     
@@ -113,8 +134,22 @@ export const createStore = (initial) => {
         return subscribers.delete(handler);
     };
 
+    const _trigger_subscribers = (next, prev) => {
+        subscribers.forEach(
+            (handler) => { 
+                try {                       // Subscriber failure shouldn't break state updates
+                    handler(next, prev); 
+                } catch (error){
+                    console.error("Subscriber handler function failed on dispatch.", error);
+                };
+            }
+        );
+    };
+
     return {
         dispatch,
+        rewind,
+        forward,
         getState,
         subscribe,
         unsubscribe
