9c34cc9f24fa56a27ef42f8ceb76f99ec7113ae5
Chris Koorzen <fckoorzen@gmail.com>
2024-11-04 11:28:25 +0200

    Add JSDoc annotations

--- store.js
+++ store.js
@@ -1,9 +1,53 @@
+/**
+ * @typedef {Object} GenericStore A generic store that keeps track of application state.
+ * @prop {function(Dispatcher): [CurrentState, PreviousState]} dispatch Triggers the passed user-defined Dispatcher. Returns CurrentState and PreviousState.
+ * @prop {function} getState Returns the current state of the application.
+ * @prop {function} subscribe Registers a Subscriber.
+ * @prop {function} unsubscribe Removes a Subscriber.
+ */
+/**
+ * @typedef {Object} CurrentState
+ * A user-defined object that represents the current state of the application
+ */
 
+/**
+ * @typedef {Object} PreviousState
+ * A user-defined object that represents the previous state of the application
+ */
+/**
+ * A user-defined function that accepts the current state of the application, modifies it in some way (or not)
+ * and returns the updated state of the application.
+ * @callback Dispatcher
+ * @param {CurrentState} currentState
+ * @returns {CurrentState}
+ */
+/**
+ * A callback function that wants to stay updated on any changes in application state. On state change, the current and previous
+ * (immutable) states of the application is passed to this callback.
+ * @callback Subscriber
+ * @param {CurrentState} currentState
+ * @param {PreviousState} previousState
+ */
+
+
+/**
+ * Factory function to create GenericStore objects. A user-defined application state object is passed to initialize the store.
+ *
+ * @constructs GenericStore
+ * @param {Object} initial
+ */
 export const createStore = (initial) => {
 
     const states = [Object.freeze(initial)];
     const subscribers = new Set();
 
+    
+    /**
+     * Triggers the passed user-defined Dispatcher. Returns CurrentState and PreviousState.
+     *
+     * @param {Dispatcher} action
+     * @returns {[CurrentState, PreviousState]}
+     */
     const dispatch = (action) => {
         if(typeof action !== "function"){
             throw new Error("action is required to be a function");
@@ -28,10 +72,23 @@ export const createStore = (initial) => {
         return [next, prev];
     };
 
+    
+    /**
+     * Returns the current state of the application.
+     *
+     * @returns {CurrentState}
+     */
     const getState = () => {
         return states[states.length -1];
     };
 
+    
+    /**
+     * Registers a Subscriber.
+     *
+     * @param {Subscriber} handler
+     * @returns {unsubscribe(handler:Subscriber)}
+     */
     const subscribe = (handler) => {
         if(typeof handler !== "function"){
             throw new Error("handler is required to be a function");
@@ -46,6 +103,13 @@ export const createStore = (initial) => {
         return removeSubscriber;
     };
 
+    
+    /**
+     * Removes a Subscriber.
+     * 
+     * @param {Subscriber} handler
+     * @returns {Boolean}
+     */
     const unsubscribe = (handler) => {
         return subscribers.delete(handler);
     };
--- tally.js
+++ tally.js
@@ -1,20 +1,32 @@
-
+/**
+ * Define application state object structure.
+ * @param count Keeps track of the count.
+ */
 export const initialCounter = {
     count: 0
 };
 
+/**
+ * A method to add one to the count.
+ */
 export const add = (counter) => {
     return {
         count: counter.count +1
     };
 };
 
+/**
+ * A method to remove one from the count.
+ */
 export const subtract = (counter) => {
     return {
         count: counter.count -1
     };
 };
 
+/**
+ * A method to reset the counter to zero.
+ */
 export const reset = (counter) => {
     return {
         count: 0
